7936fa782f10768d81667866f5b40b6c5e87b37d
PriyankaPaul03 <priyankapaul.jobs@gmail.com>
2026-09-11 01:50:59 +0530

    updated config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? '50%' : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ["list"],
@@ -31,7 +31,7 @@ export default defineConfig({
 
   use: {
     baseURL: process.env.BASEURL,
-    headless: false,
+    headless: !process.env.CI ? false : true,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'on-first-retry',
